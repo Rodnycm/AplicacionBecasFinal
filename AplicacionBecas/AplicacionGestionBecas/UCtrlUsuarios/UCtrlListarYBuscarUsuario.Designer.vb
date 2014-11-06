d071d8af8c1d17b4c4adb6b80f209d487040711a
Backi-g <ggutierrezc@ucenfotec.ac.cr>
2014-11-06 14:53:26 -0600

    Cambios listar Usuario

--- AplicacionBecas/AplicacionGestionBecas/UCtrlUsuarios/UCtrlListarYBuscarUsuario.Designer.vb
+++ AplicacionBecas/AplicacionGestionBecas/UCtrlUsuarios/UCtrlListarYBuscarUsuario.Designer.vb
@@ -25,15 +25,12 @@ Partial Class UctrlListarYBuscarUsuario
         Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim DataGridViewCellStyle6 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim DataGridViewCellStyle7 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(UctrlListarYBuscarUsuario))
         Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
         Dim DataGridViewCellStyle5 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(UctrlListarYBuscarUsuario))
         Me.dgUsuarios = New System.Windows.Forms.DataGridView()
-        Me.btnCrearUsuario = New System.Windows.Forms.Button()
-        Me.txtBuscar = New System.Windows.Forms.TextBox()
-        Me.PbUsuarios = New System.Windows.Forms.PictureBox()
         Me.Identificación = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Nombre = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.tel = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -42,8 +39,10 @@ Partial Class UctrlListarYBuscarUsuario
         Me.RolAsignado = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.CorreoElect = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.opciones = New System.Windows.Forms.DataGridViewComboBoxColumn()
+        Me.txtBuscar = New System.Windows.Forms.TextBox()
+        Me.btnCrearUsuario = New System.Windows.Forms.Button()
+        Me.PbUsuarios = New System.Windows.Forms.PictureBox()
         Me.btnVolver = New System.Windows.Forms.Button()
-
         CType(Me.dgUsuarios, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PbUsuarios, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -56,7 +55,7 @@ Partial Class UctrlListarYBuscarUsuario
         Me.dgUsuarios.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
         DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter
         DataGridViewCellStyle1.BackColor = System.Drawing.Color.Maroon
-        DataGridViewCellStyle1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Segoe UI Light", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
         DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
         DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
@@ -74,7 +73,6 @@ Partial Class UctrlListarYBuscarUsuario
         Me.dgUsuarios.DefaultCellStyle = DataGridViewCellStyle6
         Me.dgUsuarios.GridColor = System.Drawing.Color.White
         Me.dgUsuarios.Location = New System.Drawing.Point(42, 233)
-
         Me.dgUsuarios.Name = "dgUsuarios"
         Me.dgUsuarios.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
         DataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
@@ -89,42 +87,9 @@ Partial Class UctrlListarYBuscarUsuario
         Me.dgUsuarios.Size = New System.Drawing.Size(947, 271)
         Me.dgUsuarios.TabIndex = 16
         '
-        'btnCrearUsuario
-        '
-        Me.btnCrearUsuario.BackgroundImage = CType(resources.GetObject("btnCrearUsuario.BackgroundImage"), System.Drawing.Image)
-        Me.btnCrearUsuario.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnCrearUsuario.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCrearUsuario.ForeColor = System.Drawing.Color.White
-        Me.btnCrearUsuario.Location = New System.Drawing.Point(783, 108)
-        Me.btnCrearUsuario.Name = "btnCrearUsuario"
-        Me.btnCrearUsuario.Size = New System.Drawing.Size(222, 79)
-        Me.btnCrearUsuario.TabIndex = 19
-        Me.btnCrearUsuario.Text = "Crear Usuario"
-        Me.btnCrearUsuario.TextAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.btnCrearUsuario.UseVisualStyleBackColor = True
-        '
-        'txtBuscar
-        '
-        Me.txtBuscar.Font = New System.Drawing.Font("Segoe UI Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtBuscar.Location = New System.Drawing.Point(31, 160)
-        Me.txtBuscar.Name = "txtBuscar"
-        Me.txtBuscar.Size = New System.Drawing.Size(746, 29)
-        Me.txtBuscar.TabIndex = 21
-        Me.txtBuscar.Text = "Buscar:"
-        '
-        'PbUsuarios
-        '
-        Me.PbUsuarios.BackgroundImage = CType(resources.GetObject("PbUsuarios.BackgroundImage"), System.Drawing.Image)
-        Me.PbUsuarios.Image = CType(resources.GetObject("PbUsuarios.Image"), System.Drawing.Image)
-        Me.PbUsuarios.Location = New System.Drawing.Point(30, 202)
-        Me.PbUsuarios.Name = "PbUsuarios"
-        Me.PbUsuarios.Size = New System.Drawing.Size(975, 321)
-        Me.PbUsuarios.TabIndex = 18
-        Me.PbUsuarios.TabStop = False
-        '
         'Identificación
         '
-        DataGridViewCellStyle2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Segoe UI Light", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Identificación.DefaultCellStyle = DataGridViewCellStyle2
         Me.Identificación.FillWeight = 101.7639!
         Me.Identificación.HeaderText = "Identificación"
@@ -132,7 +97,7 @@ Partial Class UctrlListarYBuscarUsuario
         '
         'Nombre
         '
-        DataGridViewCellStyle3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle3.Font = New System.Drawing.Font("Segoe UI Light", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Nombre.DefaultCellStyle = DataGridViewCellStyle3
         Me.Nombre.FillWeight = 271.8829!
         Me.Nombre.HeaderText = "Nombre"
@@ -159,11 +124,11 @@ Partial Class UctrlListarYBuscarUsuario
         'RolAsignado
         '
         Me.RolAsignado.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader
-        DataGridViewCellStyle4.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        DataGridViewCellStyle4.Font = New System.Drawing.Font("Segoe UI Light", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.RolAsignado.DefaultCellStyle = DataGridViewCellStyle4
         Me.RolAsignado.HeaderText = "Rol Asignado"
         Me.RolAsignado.Name = "RolAsignado"
-        Me.RolAsignado.Width = 111
+        Me.RolAsignado.Width = 99
         '
         'CorreoElect
         '
@@ -173,7 +138,7 @@ Partial Class UctrlListarYBuscarUsuario
         '
         'opciones
         '
-        DataGridViewCellStyle5.Font = New System.Drawing.Font("Segoe UI", 9.75!)
+        DataGridViewCellStyle5.Font = New System.Drawing.Font("Segoe UI Light", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.opciones.DefaultCellStyle = DataGridViewCellStyle5
         Me.opciones.FillWeight = 126.3532!
         Me.opciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat
@@ -182,7 +147,8 @@ Partial Class UctrlListarYBuscarUsuario
         Me.opciones.Name = "opciones"
         Me.opciones.Resizable = System.Windows.Forms.DataGridViewTriState.[True]
         Me.opciones.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic
-        '        'txtBuscar
+        '
+        'txtBuscar
         '
         Me.txtBuscar.Font = New System.Drawing.Font("Segoe UI Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtBuscar.Location = New System.Drawing.Point(30, 158)
@@ -230,7 +196,6 @@ Partial Class UctrlListarYBuscarUsuario
         Me.btnVolver.UseVisualStyleBackColor = False
         Me.btnVolver.Visible = False
         '
-
         'UctrlListarYBuscarUsuario
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -241,7 +206,6 @@ Partial Class UctrlListarYBuscarUsuario
         Me.Controls.Add(Me.btnCrearUsuario)
         Me.Controls.Add(Me.txtBuscar)
         Me.Controls.Add(Me.PbUsuarios)
-        Me.Location = New System.Drawing.Point(145, 50)
         Me.Name = "UctrlListarYBuscarUsuario"
         Me.Size = New System.Drawing.Size(1037, 578)
         CType(Me.dgUsuarios, System.ComponentModel.ISupportInitialize).EndInit()
@@ -254,7 +218,6 @@ Partial Class UctrlListarYBuscarUsuario
     Friend WithEvents btnCrearUsuario As System.Windows.Forms.Button
     Friend WithEvents dgUsuarios As System.Windows.Forms.DataGridView
     Friend WithEvents txtBuscar As System.Windows.Forms.TextBox
-
     Friend WithEvents Identificación As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Nombre As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents tel As System.Windows.Forms.DataGridViewTextBoxColumn
@@ -264,6 +227,5 @@ Partial Class UctrlListarYBuscarUsuario
     Friend WithEvents CorreoElect As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents opciones As System.Windows.Forms.DataGridViewComboBoxColumn
     Friend WithEvents btnVolver As System.Windows.Forms.Button
-
 
 End Class
--- AplicacionBecas/AplicacionGestionBecas/UCtrlUsuarios/UCtrlListarYBuscarUsuario.vb
+++ AplicacionBecas/AplicacionGestionBecas/UCtrlUsuarios/UCtrlListarYBuscarUsuario.vb
@@ -1,15 +1,9 @@
 ﻿Imports EntitiesLayer
-
 Public Class UctrlListarYBuscarUsuario
-
-
     Dim ucntrlUsuario As UctrlCrearUsuario = New UctrlCrearUsuario()
-    Dim ctrlUsuario As UCtrlConsultarUsuario = New UCtrlConsultarUsuario()
-
+  
     Public Sub UctrlListarYBuscarUsuario_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
         listarUsuarios()
-
     End Sub
 
     '<summary> Método que se encarga de listar los usuarios que hay registrados en el sistema</summary>
@@ -23,11 +17,11 @@ Public Class UctrlListarYBuscarUsuario
             listaUsuarios = objGestorUsuario.buscarUsuarios()
 
             For Each usuario As Usuario In listaUsuarios
-                dgUsuarios.Rows.Add(usuario.identificacion, usuario.primerNombre & " " & usuario.primerApellido & " " & usuario.segundoApellido, usuario.rol.Nombre)
+                dgUsuarios.Rows.Add(usuario.identificacion, usuario.primerNombre & " " & usuario.primerApellido & " " & usuario.segundoApellido, usuario.telefono, usuario.fechaNacimiento, "", usuario.rol.Nombre, usuario.correoElectronico)
             Next
         Catch ex As Exception
             Dim uctrlAlerta As UctrlAlerta = New UctrlAlerta()
-            Me.Controls.Add(uctrlAlerta)
+            Me.Controls.Add(ucntrlUsuario)
             uctrlAlerta.Location = New Point(300, 100)
             uctrlAlerta.BringToFront()
             uctrlAlerta.lblAlerta.Text = "No hay usuarios registrados"
@@ -35,12 +29,10 @@ Public Class UctrlListarYBuscarUsuario
 
 
         End Try
-
+           
     End Sub
 
     Public Sub btnCrearUsuario_Click(sender As Object, e As EventArgs) Handles btnCrearUsuario.Click
-
-        ucntrlUsuario = New UctrlCrearUsuario()
         FrmIniciarSesion.principal.Controls.Add(ucntrlUsuario)
         ucntrlUsuario.setLista(Me)
         ucntrlUsuario.Location = New Point(300, 100)
@@ -68,11 +60,27 @@ Public Class UctrlListarYBuscarUsuario
     '<param name = "parametro"> variable que almacena el parametro con la identificacion del usuario que se desea consultar </param>
     '<returns> No retorna valor.</returns> 
     Public Sub consultarUsuario(ByVal pparametro As String)
-        Me.Hide()
-        ctrlUsuario.setParametro(pparametro)
-        FrmIniciarSesion.principal.Controls.Add(ctrlUsuario)
-        ctrlUsuario.Location = New Point(120, 0)
-        ctrlUsuario.Show()
+        Dim objUsuario As Usuario = objGestorUsuario.buscarUnUsuario(pparametro)
+       
+        Dim genero As String
+        dgUsuarios.Rows.Clear()
+
+        If objUsuario.genero = 1 Then
+            genero = "Masculino"
+        ElseIf objUsuario.genero = 2 Then
+            genero = "Femenino"
+        Else
+            genero = "Otro"
+        End If
+
+        dgUsuarios.Columns("tel").Visible = True
+        dgUsuarios.Columns("Fecha").Visible = True
+        dgUsuarios.Columns("gen").Visible = True
+        dgUsuarios.Columns("CorreoElect").Visible = True
+        dgUsuarios.Columns("opciones").Visible = False
+        btnVolver.Visible = True
+
+        dgUsuarios.Rows.Add(objUsuario.identificacion, objUsuario.primerNombre & " " & objUsuario.primerApellido & " " & objUsuario.segundoApellido, objUsuario.telefono, objUsuario.fechaNacimiento, genero, objUsuario.rol.Nombre, objUsuario.correoElectronico)
 
     End Sub
 
@@ -83,9 +91,7 @@ Public Class UctrlListarYBuscarUsuario
     Public Sub eliminarUsuario(ByVal parametro As String)
         Dim ucntrl As UctrlEliminarUsuario = New UctrlEliminarUsuario()
         ucntrl.setParametro(parametro)
-
         ucntrl.refrescarLista(Me)
-
         'FrmIniciarSesion.principal.Controls.Add(ctrlUsuario)
         Me.Controls.Add(ucntrl)
         Me.dgUsuarios.SendToBack()
@@ -93,7 +99,6 @@ Public Class UctrlListarYBuscarUsuario
         ucntrl.BringToFront()
         ucntrl.Location = New Point(280, 250)
         ucntrl.Show()
-
     End Sub
 
     '<summary> Método que se encarga de modificar un usuario del sistema</summary>
@@ -134,33 +139,11 @@ Public Class UctrlListarYBuscarUsuario
         End If
     End Sub
 
-    Private Sub txtBuscar_TextChanged(sender As Object, e As EventArgs) Handles txtBuscar.MouseClick
-
-        txtBuscar.Text = ""
-
-    End Sub
-
-    ''' <summary>Cuando el evento se ejecuta al dar presionar la tecla enter llama al metodo eliminar carrera</summary>
-    ''' <autor>Alvaro Artavia</autor>
-
-    Private Sub txtBuscar_KeyDown(sender As Object, e As System.Windows.Forms.KeyEventArgs) Handles txtBuscar.KeyDown
-
-        Dim param As String = txtBuscar.Text
-
-        If e.KeyCode = 13 Then
-
-            buscarUsuario(param)
-
-        End If
-
-    End Sub
-
-    Public Sub buscarUsuario(ByVal param As String)
-
+    Private Sub txtBuscar_TextChanged(sender As Object, e As EventArgs) Handles txtBuscar.TextChanged, txtBuscar.VisibleChanged
         Try
-            Dim objUsuario As Usuario = objGestorUsuario.buscarUnUsuario(param)
+            Dim objUsuario As Usuario = objGestorUsuario.buscarUnUsuario(txtBuscar.Text)
             dgUsuarios.Rows.Clear()
-            dgUsuarios.Rows.Add(objUsuario.identificacion, objUsuario.primerNombre & " " & objUsuario.primerApellido & " " & objUsuario.segundoApellido, objUsuario.rol.Nombre)
+            dgUsuarios.Rows.Add(objUsuario.identificacion, objUsuario.primerNombre & " " & objUsuario.primerApellido & " " & objUsuario.segundoApellido, objUsuario.telefono, objUsuario.fechaNacimiento, "", objUsuario.rol.Nombre, objUsuario.correoElectronico)
         Catch ex As Exception
             dgUsuarios.Rows.Clear()
             listarUsuarios()
@@ -169,6 +152,21 @@ Public Class UctrlListarYBuscarUsuario
     End Sub
 
 
+    Private Sub btnVolver_Click(sender As Object, e As EventArgs) Handles btnVolver.Click
+        dgUsuarios.Columns("tel").Visible = False
+        dgUsuarios.Columns("Fecha").Visible = False
+        dgUsuarios.Columns("gen").Visible = False
+        dgUsuarios.Columns("CorreoElect").Visible = False
+        dgUsuarios.Columns("opciones").Visible = True
+        btnVolver.Visible = False
+        dgUsuarios.Rows.Clear()
+        listarUsuarios()
+
+    End Sub
+
+    Private Sub dgUsuarios_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgUsuarios.CellContentClick
+
+    End Sub
 End Class
 
 

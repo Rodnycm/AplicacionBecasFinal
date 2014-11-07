787c9b2852fb6e2f4fcb3ef8bb200b0cf823906a
valeram <valeram96@gmail.com>
2014-11-07 11:55:58 -0600

    Valeram96

--- AplicacionBecas/DAL/Repositories/CursoRepository.cs
+++ AplicacionBecas/DAL/Repositories/CursoRepository.cs
@@ -104,6 +104,25 @@ namespace DAL{
             }
         }
 
+        //<summary> Método que se encarga de traer de la base de datos todos los cursos registrados </summary>
+        //<author>Valeria Ramírez Cordero</author> 
+        //<param> no recibe parametros </param>
+        //<returns>Retorna una lista con todos los cursos registrados en el sistema.</returns> 
+        //public IEnumerable<Curso> GetCuatrimestres(){
+        //    try{
+        //        DataSet ds = DBAccess.ExecuteSPWithDS(ref cmd, "Sp_buscarCuatrimestre")
+        //    }
+        //    catch (SqlException ex)
+        //    {
+        //        numero = ex.Number;
+        //        mensaje = exceptions.validarExcepcion(numero);
+        //        throw new CustomExceptions.DataAccessException(mensaje, ex);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
         //<summary> Método que se encarga de traer de la base de datos un curso en específico </summary>
         //<author>Valeria Ramírez Cordero</author> 
         //<param name "id"> parámetro de tipo int que contiene el Id del curso que se desea traer </param>
